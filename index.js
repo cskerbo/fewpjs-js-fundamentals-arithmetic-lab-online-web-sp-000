8a21a31f007bc07170b496399acd57782a977972
Chris Kerbo <chris.kerbo@outlook.com>
2020-03-24 00:03:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,5 +6,8 @@ const multiply = num1 * num2
 // generates a random integer greater than 0
 let random = Math.random() + 1
 
-//
+// generates a remainder that is equal to 4
 const mod = 16 % 6
+
+// will return 20 as the highest number in the set
+const max = Math.max(5, 10, 15, 20)
